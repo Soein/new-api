68cc117c53741823d81fdf4374bab0324c85bdd3
pzy <2360718056@qq.com>
2026-02-24 15:24:24 +0800

    deploy: 添加自定义部署补丁
    - Dockerfile: 添加 GOPROXY 加速 Go 模块下载
    - stream_scanner: channel 写入超时从 10s 调整为 600s，适配思考模型长推理场景

--- relay/helper/stream_scanner.go
+++ relay/helper/stream_scanner.go
@@ -153,7 +153,7 @@ func StreamScannerHandler(c *gin.Context, resp *http.Response, info *relaycommon
 						if common.DebugEnabled {
 							println("ping data sent")
 						}
-					case <-time.After(10 * time.Second):
+					case <-time.After(600 * time.Second):
 						logger.LogError(c, "ping data send timeout")
 						return
 					case <-ctx.Done():
@@ -235,7 +235,7 @@ func StreamScannerHandler(c *gin.Context, resp *http.Response, info *relaycommon
 					if !success {
 						return
 					}
-				case <-time.After(10 * time.Second):
+				case <-time.After(600 * time.Second):
 					logger.LogError(c, "data handler timeout")
 					return
 				case <-ctx.Done():
